72ba817fc8f842801e15987c4bc493fb7154592c
bibizhou <41883318+bibizhou@users.noreply.github.com>
2020-04-09 15:44:18 +0800

    Add files via upload

---
+++ checkspamtext/src/test/java/com/example/checkspamtext/CheckspamtextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkspamtext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckspamtextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
